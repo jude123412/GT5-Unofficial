7179fb571f804019632173f194622cf548129571
jude123412 <104434396+jude123412@users.noreply.github.com>
2025-10-28 08:32:34 +0000

    Update MixinIc2NuclearReactorSound.java

--- src/mixin/java/gregtech/mixin/mixins/late/ic2/MixinIc2NuclearReactorSound.java
+++ src/mixin/java/gregtech/mixin/mixins/late/ic2/MixinIc2NuclearReactorSound.java
@@ -28,55 +28,57 @@ public abstract class MixinIc2NuclearReactorSound extends TileEntityInventory {
 
     @Inject(method = "updateEntityServer", at = @At("RETURN"), cancellable = true, remap = false)
     public void gT5_Unofficial$fixReactorSound(CallbackInfo ci) {
-        boolean playAudio = false;
-        // Check reactor is running before starting the loop
-        if (getReactorEnergyOutput() > 0.0F) {
-            int limit = (int) (getWorld().getWorldTime() % 63);
+        if (!getWorld().isRemote) {
+            boolean playAudio = false;
+            // Check reactor is running before starting the loop
+            if (getReactorEnergyOutput() > 0.0F) {
+                int limit = (int) (getWorld().getWorldTime() % 63);
 
-            // Check every 62 ticks to run audio
-            if (limit == 62) {
-                playAudio = true;
-            }
-        }
-
-        if (getReactorEnergyOutput() > 0.0F && playAudio) {
-
-            float euFactor = 0.2f;
-            float euLow = euFactor * 512; // 1amp HV
-            float euMid = euFactor * 2048; // 1amp EV
-            float euHigh = euFactor * 8192; // 1amp IV
-
-            if (getReactorEnergyOutput() < euLow) {
-                GTUtility.sendSoundToPlayers(
-                    getWorld(),
-                    SoundResource.IC2_REACTOR_LOW_LOOP,
-                    0.2F,
-                    1.0F,
-                    getPosition().posX,
-                    getPosition().posY,
-                    getPosition().posZ);
+                // Check every 62 ticks to run audio
+                if (limit == 62) {
+                    playAudio = true;
+                }
             }
 
-            if (getReactorEnergyOutput() >= euMid && getReactorEnergyOutput() < euHigh) {
-                GTUtility.sendSoundToPlayers(
-                    getWorld(),
-                    SoundResource.IC2_REACTOR_MID_LOOP,
-                    0.2F,
-                    1.0F,
-                    getPosition().posX,
-                    getPosition().posY,
-                    getPosition().posZ);
-            }
+            if (getReactorEnergyOutput() > 0.0F && playAudio) {
 
-            if (getReactorEnergyOutput() >= euHigh) {
-                GTUtility.sendSoundToPlayers(
-                    getWorld(),
-                    SoundResource.IC2_REACTOR_HIGH_LOOP,
-                    0.2F,
-                    1.0F,
-                    getPosition().posX,
-                    getPosition().posY,
-                    getPosition().posZ);
+                float euFactor = 0.2f;
+                float euLow = euFactor * 512; // 1amp HV
+                float euMid = euFactor * 2048; // 1amp EV
+                float euHigh = euFactor * 8192; // 1amp IV
+
+                if (getReactorEnergyOutput() < euLow) {
+                    GTUtility.sendSoundToPlayers(
+                        getWorld(),
+                        SoundResource.IC2_REACTOR_LOW_LOOP,
+                        0.2F,
+                        1.0F,
+                        getPosition().posX,
+                        getPosition().posY,
+                        getPosition().posZ);
+                }
+
+                if (getReactorEnergyOutput() >= euMid && getReactorEnergyOutput() < euHigh) {
+                    GTUtility.sendSoundToPlayers(
+                        getWorld(),
+                        SoundResource.IC2_REACTOR_MID_LOOP,
+                        0.2F,
+                        1.0F,
+                        getPosition().posX,
+                        getPosition().posY,
+                        getPosition().posZ);
+                }
+
+                if (getReactorEnergyOutput() >= euHigh) {
+                    GTUtility.sendSoundToPlayers(
+                        getWorld(),
+                        SoundResource.IC2_REACTOR_HIGH_LOOP,
+                        0.2F,
+                        1.0F,
+                        getPosition().posX,
+                        getPosition().posY,
+                        getPosition().posZ);
+                }
             }
         }
     }
