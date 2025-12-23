aafbc4fc9688bb40ac1ce5778ed6083de62ed2e4
jude123412 <104434396+jude123412@users.noreply.github.com>
2025-12-23 15:55:19 +0000

    Allow GT++ Hot Ingots to inherit texture from texture set?

--- src/main/java/gtPlusPlus/core/item/base/ingots/BaseItemIngotHot.java
+++ src/main/java/gtPlusPlus/core/item/base/ingots/BaseItemIngotHot.java
@@ -1,24 +1,18 @@
 package gtPlusPlus.core.item.base.ingots;
 
 import static gregtech.api.enums.Mods.GTPlusPlus;
-import static gregtech.api.enums.Mods.GregTech;
 import static gregtech.api.recipe.RecipeMaps.vacuumFreezerRecipes;
 import static gregtech.api.util.GTRecipeBuilder.TICKS;
 
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.enums.GTValues;
 import gregtech.api.util.GTUtility;
 import gtPlusPlus.api.objects.Logger;
 import gtPlusPlus.core.material.Material;
-import gtPlusPlus.core.util.Utils;
 
 public class BaseItemIngotHot extends BaseItemIngot {
 
@@ -26,9 +20,6 @@ public class BaseItemIngotHot extends BaseItemIngot {
     private final int tickCounter = 0;
     private final int tickCounterMax = 200;
     private final int mTier;
-
-    private IIcon base;
-    private IIcon overlay;
 
     public BaseItemIngotHot(final Material material) {
         super(material, ComponentTypes.HOTINGOT);
@@ -42,11 +33,6 @@ public class BaseItemIngotHot extends BaseItemIngot {
     public String getItemStackDisplayName(final ItemStack p_77653_1_) {
         return super.getItemStackDisplayName(p_77653_1_);
         // return ("Hot "+this.materialName+ " Ingot");
-    }
-
-    @Override
-    public int getColorFromItemStack(final ItemStack stack, final int HEX_OxFFFFFF) {
-        return Utils.rgbtoHexValue(225, 225, 225);
     }
 
     private void generateRecipe() {
@@ -67,28 +53,5 @@ public class BaseItemIngotHot extends BaseItemIngot {
             GTUtility.applyHeatDamageFromItem(player, 1, stack);
         }
         super.onUpdate(stack, world, entity, p_77663_4_, p_77663_5_);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean requiresMultipleRenderPasses() {
-        return true;
-    }
-
-    @Override
-    public void registerIcons(final IIconRegister i) {
-        this.base = i.registerIcon(GregTech.ID + ":" + "materialicons/METALLIC/" + "ingotHot");
-        this.overlay = i.registerIcon(GregTech.ID + ":" + "materialicons/METALLIC/" + "ingotHot_OVERLAY");
-    }
-
-    @Override
-    public IIcon getIconFromDamageForRenderPass(final int damage, final int pass) {
-        if (pass == 0) {
-            return this.base;
-        } else if (pass == 1) {
-            return this.overlay;
-        } else {
-            return this.overlay;
-        }
     }
 }
